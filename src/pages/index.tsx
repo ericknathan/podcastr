d9a6581b4a179264e943077ac5f8c3ac9243bea4
ericknathan <erick.capito@hotmail.com>
2022-10-21 11:25:18 -0300

    temp: testing static server generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
-  return <h1>Hello world</h1>;
+const Home: NextPage = ({ episodes }: any) => {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(episodes)}</p>
+    </div>
+  );
 };
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60, // 1 hour
+  };
+}
 
 export default Home;
