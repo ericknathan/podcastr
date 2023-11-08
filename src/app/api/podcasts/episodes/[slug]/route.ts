20241b38a32e77d747a54d7afc8eaf4ed74259ae
Erick Nathan <erick.capito@pagtel.com.br>
2023-11-08 16:33:57 -0300

    fix: type "undefined" is not a valid type for the function's first argument on get function

--- src/app/api/podcasts/episodes/[slug]/route.ts
+++ src/app/api/podcasts/episodes/[slug]/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 import episodes from "../database.json";
 
-export function GET(_: undefined, { params }: { params: { slug: string } }) {
+export function GET(_: NextRequest, { params }: { params: { slug: string } }) {
   const episode = episodes.find((episode) => episode.id === params.slug);
   return NextResponse.json(episode);
 }
